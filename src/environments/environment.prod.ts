deab1a2f7859da50c245b03a4b8711974d353766
Gusztavo <lvillena@everis.com>
2022-09-07 11:38:33 -0500

    change(version): 100

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   dev: {
     cdn: "https://bpi-video-firma.azureedge.net",
-    version: "0.0.99",
+    version: "0.0.100",
   },
   uat: {
     cdn: "https://cdneu2c001fdcuat01.azureedge.net",
